4b8630ff1474731c37dda2db151bda81ded26e19
Fred56733 <fdebiase2020@fau.edu>
2025-01-06 18:37:49 -0500

    FInished Challenges

--- index.js
+++ index.js
@@ -165,7 +165,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, secondTopGame] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameElement = document.createElement("p");
+topGameElement.innerHTML = topGame.name;
+firstGameContainer.appendChild(topGameElement);
 
 // do the same for the runner up item
+const secondGameElement = document.createElement("p");
+secondGameElement.innerHTML = secondTopGame.name;
+secondGameContainer.appendChild(secondGameElement);
